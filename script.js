f85b76708ef0912665288fcfbb5978936a6524cc
venous666 <99412095+venous666@users.noreply.github.com>
2022-09-06 16:11:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+jesus=[]
+
+
+function vailogo() {
+    sim= document.getElementById("maranhao").value
+ok= document.getElementById("roraima").value
+resultado= document.getElementById("brasilia")
+amazonas = parseFloat(sim)**2 + parseFloat(ok)**2
+resultado.value = Math.sqrt(amazonas)
+jesus.push
+console.log(resultado.value)
+    
+}
+function limpar(){
+    sim=document.getElementById("maranhao").value=""
+    ok=document.getElementById("roraima").value=""
+    resultado=document.getElementById("brasilia").value=""
+    jesus.push
+    console.log(resultado);
+}
+
